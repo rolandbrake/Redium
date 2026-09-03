abcc532fe30b933e2fe516618e8c44513f7d3054
Roland Brake <rolandbrake@hotmail.com>
2026-09-03 03:18:46 +0300

    Stop tracking generated files

--- dist/types/src/layout/Center.d.ts
+++
@@ -1,7 +0,0 @@
-import { Container, type ContainerOptions } from "../elements/Container.js";
-import type { Element } from "../core/Element.js";
-export type CenterOptions = Omit<ContainerOptions, "children" | "row" | "center">;
-export type Center = ReturnType<typeof Container>;
-/** A single-child wrapper that fills its parent and centers its child. */
-export declare const Center: (child: Element, options?: CenterOptions) => import("../elements/Container.js").ContainerElement;
-//# sourceMappingURL=Center.d.ts.map
--- dist/types/src/layout/Column.d.ts
+++
@@ -1,7 +0,0 @@
-import { Container, type ContainerOptions } from "../elements/Container.js";
-export interface ColumnOptions extends ContainerOptions {
-}
-export type Column = ReturnType<typeof Container>;
-/** Convenience wrapper for the default vertical Container. */
-export declare const Column: (options?: ColumnOptions) => import("../elements/Container.js").ContainerElement;
-//# sourceMappingURL=Column.d.ts.map
